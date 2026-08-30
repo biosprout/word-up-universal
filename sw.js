b4e0cfe0c0e93061d73bec3c5acbb546199bd5bb
biosprout <yucciyucci+bio@gmail.com>
2026-08-30 10:43:03 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+// WORD UP! Service Worker
+// 一度読み込んだファイルをキャッシュし、オフラインでも動くようにする
+const CACHE = 'wordup-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-180.png',
+  './data/index.json',
+  './data/m1.json',
+  './data/m2.json',
+  './data/m3.json',
+  './data/p2.json',
+  './data/e2.json',
+  './data/p1.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS).catch(() => {}))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// ネットワーク優先。失敗したらキャッシュを返す（更新を取りこぼさないため）
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== location.origin) return;   // フォントなど外部は素通し
+  e.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
